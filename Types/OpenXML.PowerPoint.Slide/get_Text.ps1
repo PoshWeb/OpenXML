5cfa47faaddc8f0849b9eab045c0d5a53eae9592
James Brundage <+@noreply.github.com>
2025-10-14 13:32:40 -0700

    feat: OpenXML.PowerPoint.Slide.get_Text ( Fixes #10 )

---
+++ Types/OpenXML.PowerPoint.Slide/get_Text.ps1
@@ -0,0 +1,9 @@
+@($this.Content | 
+    Select-Xml -XPath '//a:t' -Namespace @{a='http://schemas.openxmlformats.org/drawingml/2006/main'} |
+    Foreach-Object {
+        if ($_.Node.LocalName -eq 't') {
+            $_.Node.InnerText  
+        } else {
+            [Environment]::NewLine
+        }        
+    }) -join ''
