4df7a074a5a9a8f6fdba3a6748ff6aaa817f1cf3
StartAutomating <james.brundage@start-automating.com>
2026-02-15 23:07:23 -0800

    feat: OpenXML.Word.File.get_Document ( Fixes #44 )

---
+++ Types/OpenXML.Word.File/get_Document.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+    Gets the document in a word file
+.DESCRIPTION
+    Gets the `/word/document.xml` within a Word File. 
+#>
+if ($null -ne $this.'#document') { return $this.'#document'}
+
+if (-not $this.PartExists) { return }
+if (-not $this.PartExists('/word/document.xml')) { return }
+
+$documentPart = $this.GetPart('/word/document.xml')
+
+$docStream = $documentPart.GetStream()
+
+$streamReader = [IO.StreamReader]::new($docStream)
+
+$documentXml = $streamReader.ReadToEnd() -as [xml]
+
+$streamReader.Close()
+$streamReader.Dispose()
+
+$docStream.Close()
+$docStream.Dispose()
+
+if ($documentXml) {
+    $documentXml.pstypenames.insert(0,'OpenXML.Word.Document')
+    $documentXml.psobject.properties.add(
+        [psnoteproperty]::new('FilePath', $this.FilePath), $false
+    )
+    $documentXml.psobject.properties.add(
+        [psnoteproperty]::new('OpenXML', $this), $false
+    )
+    $this.psobject.properties.add(
+        [psnoteproperty]::new('#document', $documentXml), $false
+    )    
+    return $this.'#document'
+}
+
