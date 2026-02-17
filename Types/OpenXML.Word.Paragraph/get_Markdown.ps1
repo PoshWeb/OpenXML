9ee9dc797a1d1672e65d1c67cab3817b614b1269
StartAutomating <james.brundage@start-automating.com>
2026-02-16 23:11:24 -0800

    feat: OpenXML.Word.Paragraph.get_Markdown ( Fixes #57 )

---
+++ Types/OpenXML.Word.Paragraph/get_Markdown.ps1
@@ -0,0 +1,31 @@
+$runs = @($this.r)        
+    
+@(foreach ($run in $runs) {
+    
+    $runText = $run.t
+    if ($runText -isnot [string]) {
+        $runText = $run.t.InnerText
+    }
+
+    if (-not $run.rPr.psobject.properties) {
+        $runText
+    }
+    elseif ($run.rPr.psobject.properties['b'] -and
+        $run.rPr.psobject.properties['i']
+    ) {
+        "***$runText***"
+    }
+    elseif (                
+        $run.rPr.psobject.properties['b']
+    ) {
+        "**$runText**"
+    }
+    elseif (                
+        $run.rPr.psobject.properties['i']
+    ) {
+        "*$runText*"
+    } else  {
+        $runText  
+    } 
+    
+}) -join '' -replace '(?<=\w)(?>\*{6}|\*{4})(?=\w)'
